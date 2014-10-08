f4b9f2fc944f515d8f4617b59cd26d35bdbd90e7
benjaoming <benjaoming@gmail.com>
2014-10-08 17:24:04 +0200

    Remove check on _state.adding because it fires wrongly

--- parler/models.py
+++ parler/models.py
@@ -318,7 +318,11 @@ class TranslatableModel(models.Model):
         except KeyError:
             # 2. No cache, need to query
             # Check that this object already exists, would be pointless otherwise to check for a translation.
-            if not self._state.adding and self.pk:
+            # benjaoming: Removing below, because self._state.adding is a
+            # default value that is possible even with pk set, and that is
+            # really wrong. 
+            #     if not self._state.adding and self.pk:
+            if self.pk:
                 prefetch = self._get_prefetched_translations()
                 if prefetch is not None:
                     # 2.1, use prefetched data
