ad3aab4cdd2bd2ca5af8006fb4422f8ddbed482b
Joseph Melettukunnel <jmelett@gmail.com>
2015-05-26 21:18:38 +0200

    Fix _get_cached_values to use new fallbacks logic

--- parler/cache.py
+++ parler/cache.py
@@ -123,8 +123,11 @@ def _get_cached_values(instance, translated_model, language_code, use_fallback=F
         # Allow to return the fallback language instead.
         if use_fallback:
             lang_dict = get_language_settings(language_code)
-            if lang_dict['fallbacks'] != language_code:
-                return _get_cached_values(instance, translated_model, lang_dict['fallbacks'], use_fallback=False)
+            # iterate over list of fallback languages, which should be already
+            # in proper order
+            for i_lang in lang_dict['fallbacks']:
+                if i_lang != language_code:
+                    return _get_cached_values(instance, translated_model, i_lang, use_fallback=False)
         return None
 
     values['master'] = instance
