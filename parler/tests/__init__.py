a5bff919e48e66b4de299b24483090f32bf847d5
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-16 14:19:49 +0100

    Restore Django 1.5 tests, broken by #111

---
+++ parler/tests/__init__.py
@@ -0,0 +1,10 @@
+import django
+if django.VERSION < (1, 6):
+    # Expose for Django 1.5 and below (before DiscoverRunner)
+    from .test_admin import *
+    from .test_model_construction import *
+    from .test_model_attributes import *
+    from .test_model_inheritance import *
+    from .test_forms import *
+    from .test_urls import *
+    from .test_query_count import *
