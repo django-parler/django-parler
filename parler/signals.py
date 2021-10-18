3df60ebd9bb8e19b473478d7f9721c11295abda3
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2021-10-18 20:40:55 +0200

    rm deprecated providing_args from signals (#279)
    see https://docs.djangoproject.com/en/3.1/internals/deprecation/#deprecation-removed-in-4-0

--- parler/signals.py
+++ parler/signals.py
@@ -7,11 +7,11 @@ Use the signals as last resort, or to maintain separation of concerns.
 """
 from django.dispatch import Signal
 
-pre_translation_init = Signal(providing_args=["instance", "args", "kwargs"])
-post_translation_init = Signal(providing_args=["instance"])
+pre_translation_init = Signal()
+post_translation_init = Signal()
 
-pre_translation_save = Signal(providing_args=["instance", "raw", "using"])
-post_translation_save = Signal(providing_args=["instance", "raw", "created", "using"])
+pre_translation_save = Signal()
+post_translation_save = Signal()
 
-pre_translation_delete = Signal(providing_args=["instance", "using"])
-post_translation_delete = Signal(providing_args=["instance", "using"])
+pre_translation_delete = Signal()
+post_translation_delete = Signal()
